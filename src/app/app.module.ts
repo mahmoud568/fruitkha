1e4eb93efbd60e7595b0625e84b05f26b1426672
mahmoud abdelwahab <mahmoudabdelwahab4242@gmail.com>
2022-01-26 14:58:54 +0200

    change home page name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './main-pages/header/header.component';
-import { HomeComponent } from './main-pages/home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgbAlertModule, NgbModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
