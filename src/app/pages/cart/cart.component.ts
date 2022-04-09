a8c05d67306d6530e4bf23f3855edc181870dbc2
mahmoud abdelwahab <mahmoudabdelwahab4242@gmail.com>
2022-04-09 11:09:43 +0200

    make shop responsive

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { fruit } from 'src/app/shared/Interface/fruit.model';
+import { currencyexchange } from 'src/app/shared/Interface/option.model';
+import { SharedService } from 'src/app/shared/services/shared.service';
+import { HeaderService } from '../header/service/header.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  cart: { fruit: fruit; quantity: number }[] = [];
+  currency!: string;
+  exchangerate!: number;
+  subscrition!: Subscription;
+
+  SubtotalInitialValue: number = 0
+  Subtotal: number = 0;
+  Shipping: number = 1;
+
+  constructor(private sharedService: SharedService, private headerService: HeaderService) { }
 
   ngOnInit(): void {
+    this.cart = this.sharedService.cart;
+    this.cart[0].fruit.fruitPrice;
+
+    this.Subtotal = this.cart.reduce(
+      //@ts-ignore
+      (previousValue, currentValue) => previousValue + (currentValue.fruit.fruitPrice * currentValue.quantity),
+      this.SubtotalInitialValue
+    );
+
+    this.subscrition = this.headerService.currencyChanged.subscribe(
+      (res: currencyexchange) => {
+        this.currency = res.currency;
+        this.exchangerate = res.exchangerate;
+      }
+    );
+    console.log(this.cart)
   }
 
+  ngOnDestroy(): void {
+    this.subscrition.unsubscribe();
+  }
+
+  onTypeOnInput() {
+    this.SubtotalInitialValue = 0;
+    this.Subtotal = this.cart.reduce(
+      //@ts-ignore
+      (previousValue, currentValue) => previousValue + (currentValue.fruit.fruitPrice * currentValue.quantity),
+      this.SubtotalInitialValue
+    );
+  }
 }
